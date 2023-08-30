2a83fe79ad5544ccbe2bb4cd8f33cbb4177d6595
Minkyu Kim <min.kim@ocollor.com>
2023-08-30 09:31:35 +0900

    First commit - convert to typescript

---
+++ index.d.ts
@@ -0,0 +1,53 @@
+/**
+ * @see https://github.com/shpontex/cropme
+ */
+declare module 'cropme' {
+    export type CropmeOptions = {
+        container?: {
+            width?: number | string,
+            height?: number | string,
+        },
+        viewport?: {
+            type?: string,
+            width?: number | string,
+            height?: number | string,
+            border?: {
+                enable?: boolean,
+                width?: number,
+                color?: string
+            }
+        },
+        transformOrigin?: string,
+        zoom?: {
+            min?: number,
+            max?: number,
+            enable?: boolean,
+            mouseWheel?: boolean,
+            slider?: boolean
+        },
+        customClass?: string,
+        rotation?: {
+            slider?: boolean,
+            enable?: boolean,
+            position?: string
+        }
+    }
+    export type Position = {
+        x: number;
+        y: number;
+        scale: number;
+        angle: number;
+    }
+    class Cropme {
+        constructor(el: HTMLElement, options?: CropmeOptions);
+        crop(options?: CropmeOptions): Promise;
+        reload(options?: CropmeOptions): void;
+
+        resize(): void;
+        position(): Position; 
+
+        destroy(): void;
+    }
+
+    export = Cropme;
+}
